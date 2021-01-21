08ca392e8e9f04f60aded4c925eee262299bcd90
Ertuğrul Kaan Özdemir <ertugrulkaanozdemir@gmail.com>
2021-01-21 22:38:01 +0300

    Add project files.

---
+++ OOP1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace OOP1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Product product1 = new Product()
+            {
+                Id = 1,
+                CategoryId = 2,
+                ProductName = "Masa",
+                UnitPrice = 500,
+                UnitsInStock = 3
+            };
+            Product product2 = new Product()
+            {
+                Id = 2,
+                CategoryId = 3,
+                ProductName = "Duvar Saati",
+                UnitPrice = 50,
+                UnitsInStock = 100
+            };
+            ProductManager productManager = new ProductManager();
+            productManager.Add(product1);
+        }
+    }
+}
