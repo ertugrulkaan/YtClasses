14b621d7f46f35c5f66ce70759168ea3e073c9d5
Ertuğrul Kaan Özdemir <ertugrulkaanozdemir@gmail.com>
2021-01-21 23:12:54 +0300

    20210121

---
+++ Collections/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Collections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] names = new string[] { "Engin", "Murat", "Kerem", "Halil" };
+            List<string> names2 = new List<string> { "Engin", "Murat", "Kerem", "Halil" };
+        }
+    }
+}
